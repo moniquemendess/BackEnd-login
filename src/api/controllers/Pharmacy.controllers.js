0d15a2839b4349321aeb9a4b321f33d8ac1e45c2
Monique Mendes <moniiquemendes96@gmail.com>
2024-02-07 19:03:05 +0100

    update

--- src/api/controllers/Pharmacy.controllers.js
+++ src/api/controllers/Pharmacy.controllers.js
@@ -1,4 +1,5 @@
 const Pharmacy = require("../models/Pharmacy.model");
+const Product = require("../models/Product.model");
 const { deleteImgCloudinary } = require("../../middleware/files.middleware");
 
 /** C R U D **/
@@ -52,5 +53,108 @@ const createPharmacy = async (req, res, next) => {
     return next(error);
   }
 };
+//! ---------------------------------------------------------------------
+//? ----------------------------add o delete un character  --------------
+//! ---------------------------------------------------------------------
+/// aqui metemos los productos en el array del modelo de pharmacy
+const toggleProduct = async (req, res, next) => {
+  try {
+    /** estee id es el id de la pharmacy que queremos actualizar */
+    const { id } = req.params;
+    const { products } = req.body; // -----> idDeLosProduct enviaremos esto por el req.body "12412242253,12535222232,12523266346"
+    /** Buscamos la product por id para saber si existe */
+    const pharmacyById = await Pharmacy.findById(id);
 
-module.exports = { createPharmacy };
+    /** vamos a hacer un condicional para si existee hacer la update sino mandamos un 404 */
+    if (pharmacyById) {
+      /** cageemos el string que traemos del body y lo convertimos en un array
+       * separando las posiciones donde en el string habia una coma
+       * se hace mediante el metodo del split
+       */
+      const arrayIdProducts = products.split(",");
+
+      /** recorremos este array que hemos creado y vemos si tenemos quee:
+       * 1) ----> sacar eel character si ya lo tenemos en el back
+       * 2) ----> meterlo en caso de que no lo tengamos metido en el back
+       */
+      Promise.all(
+        arrayIdProducts.map(async (product, index) => {
+          if (pharmacyById.products.includes(product)) {
+            //*************************************************************************** */
+
+            //________ BORRAR DEL ARRAY DE PERSONAJES EL PEERSONAJE DENTRO DE LA MOVIE___
+
+            //*************************************************************************** */
+
+            try {
+              await Pharmacy.findByIdAndUpdate(id, {
+                // dentro de la clavee characters me vas a sacar el id del elemento que estoy recorriendo
+                $pull: { products: product },
+              });
+
+              try {
+                // busca esse product y me hace un pull/ del array de pharmacy id
+                await Product.findByIdAndUpdate(product, {
+                  $pull: { pharmacies: id },
+                });
+              } catch (error) {
+                res.status(404).json({
+                  error: "Error updating product",
+                  message: error.message,
+                }) && next(error);
+              }
+            } catch (error) {
+              res.status(404).json({
+                error: "Error updating pharmacy",
+                message: error.message,
+              }) && next(error);
+            }
+          } else {
+            //*************************************************************************** */
+            //________ METER EL PERSONAJE EN EL ARRAY DE PERSONAJES DE LA MOVIE_____________
+            //*************************************************************************** */
+            /** si no lo incluye lo tenemos que meter -------> $push */
+
+            try {
+              await Pharmacy.findByIdAndUpdate(id, {
+                $push: { products: product },
+              });
+              try {
+                await Product.findByIdAndUpdate(product, {
+                  $push: { pharmacies: id },
+                });
+              } catch (error) {
+                res.status(404).json({
+                  error: "Error updating product",
+                  message: error.message,
+                }) && next(error);
+              }
+            } catch (error) {
+              res.status(404).json({
+                error: "Error updating pharmacy",
+                message: error.message,
+              }) && next(error);
+            }
+          }
+        })
+      )
+        .catch((error) => res.status(404).json(error.message))
+        .then(async () => {
+          return res.status(200).json({
+            dataUpdate: await Pharmacy.findById(id).populate("products"),
+          });
+        });
+    } else {
+      return res.status(404).json("This pharmacy does not exist");
+    }
+  } catch (error) {
+    return (
+      res.status(404).json({
+        error: "error catch",
+        message: error.message,
+      }) && next(error)
+    );
+  }
+};
+
+module.exports = { createPharmacy, toggleProduct };
--- src/api/routes/Pharmacy.routes.js
+++ src/api/routes/Pharmacy.routes.js
@@ -1,7 +1,10 @@
 const { upload } = require("../../middleware/files.middleware");
-const { createPharmacy } = require("../controllers/Pharmacy.controllers");
+const {
+  createPharmacy,
+  toggleProduct,
+} = require("../controllers/Pharmacy.controllers");
 const PharmacyRoutes = require("express").Router();
 
 PharmacyRoutes.post("/createPharmacy", upload.single("image"), createPharmacy);
-
+PharmacyRoutes.patch("/add/:id", toggleProduct);
 module.exports = PharmacyRoutes;
